02173137cff676df935d7969bfd1e3b413ad074d
User <ezzy_nauzzee@hotmail.com>
2018-11-19 14:17:53 +0800

    Fixed minor bug when merging with yee's feature/user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2018_11_19_052120) do
+=======
+ActiveRecord::Schema.define(version: 2018_11_19_053558) do
+>>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +25,20 @@ ActiveRecord::Schema.define(version: 2018_11_19_052120) do
     t.date "expired_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+<<<<<<< Updated upstream
+=======
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+>>>>>>> Stashed changes
   end
 
 end
