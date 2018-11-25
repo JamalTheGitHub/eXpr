98596d05dee5543ce7cdafe583213d78c3057a0a
hunshenl <hunshen.lau@gmail.com>
2018-11-25 18:10:05 +0800

    fixed search error in new view

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -179,8 +179,8 @@ class GroceriesController < ApplicationController
 
     # DDth(month)YYYY || DD(month)YYYY || Dth(month)YYYY
     months = Date::MONTHNAMES.compact
-    item_date = /\d{1,2}(st|nd|rd|th)?(\s?(Of)?)\s(#{months.join('|')})\s\d{4}/
-    date_only = /^\d{1,2}(st|nd|rd|th)?(\s?(Of)?)\s(#{months.join('|')})\s\d{4}$/
+    item_date = /\d{1,2}(st|nd|rd|th)?(\s?(Of)?)\s(#{months.join('|')})(\s\d{4})?/
+    date_only = /^\d{1,2}(st|nd|rd|th)?(\s?(Of)?)\s(#{months.join('|')})(\s\d{4})?$/
     
 
     # Capitalises all first character of words only
